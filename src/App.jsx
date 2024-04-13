a8f75d130ec6ebcd18942efdff19f26c0629b529
Jacob Lefler <jcb.lefler@gmail.com>
2024-04-13 11:30:18 -0400

    mid lecture edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import reactImg from "./assets/react-core-concepts.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
 
 function Main() {
   return (
     <main>
-      <h2>Time to get started!</h2>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts {...CORE_CONCEPTS[0]} />
+          <CoreConcepts {...CORE_CONCEPTS[1]} />
+          <CoreConcepts {...CORE_CONCEPTS[2]} />
+          <CoreConcepts
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+          />
+        </ul>
+      </section>
+      <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton>Components</TabButton>
+        </menu>
+      </section>
     </main>
   );
 }
